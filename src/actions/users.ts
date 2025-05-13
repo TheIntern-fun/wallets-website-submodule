af7c6aab732c16edbeb48316d0c1e815c87e8c37
Yash Goyal <yashgo0018@gmail.com>
2025-05-13 15:30:39 +0100

    wip

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,5 +1,6 @@
 "use server";
 import jwt from "jsonwebtoken";
+import { privy } from "@/lib/privy";
 
 export async function getUserInfo(token: string) {
   const jwtPublicKey = process.env.PRIVY_JWT_PUBLIC_KEY || "";
@@ -7,8 +8,32 @@ export async function getUserInfo(token: string) {
   try {
     const decoded = jwt.verify(token, jwtPublicKey, {
       algorithms: ["ES256"],
-    });
+    }) as {
+      sid: string;
+      iss: string;
+      iat: number;
+      aud: string;
+      sub: string;
+      exp: number;
+    };
     console.log(decoded);
+
+    await privy.importUser({
+      linkedAccounts: [
+        {
+          type: "twitter_oauth",
+          username: "",
+          subject: "",
+          name: "",
+        },
+      ],
+      createSolanaWallet: true,
+    });
+
+    const user = await privy.getUserById(decoded.sub);
+    if (user.twitter) {
+      console.log(user.twitter);
+    }
 
     return decoded;
   } catch (error) {
